3595d4627c94b8638cc31796f57fa00b6f19d46e
jubi <joobe1031@gmail.com>
2024-09-11 19:13:28 +0900

    이미지 출력

--- my_project/urls.py
+++ my_project/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 # from django.urls import path
 from django.urls import path, include
 # from mysite import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,6 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('accounts/', include('accounts.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/models.py
+++ mysite/models.py
@@ -6,6 +6,7 @@ class MainContent(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     pub_date = models.DateTimeField('date published')
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
